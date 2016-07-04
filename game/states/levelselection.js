700c2eaf95bfef88075535e4ae181b6289f93edd
Mario Groß <m.gross@micromata.de>
2016-07-04 15:50:16 +0200

    Add better button logic

--- game/states/levelselection.js
+++ game/states/levelselection.js
@@ -4,14 +4,33 @@ function LevelSelection() {}
 LevelSelection.prototype = {
   create: function() {
 
-    this.sprite = this.game.add.sprite(5, 5, 'buttonlevel1');
-    this.sprite.inputEnabled = true;
 
-    this.sprite.events.onInputDown.add(this.clickListener, this);
+    this.button = this.game.add.button(5, 5, 'buttonlevel1', this.clickListener, this, 0);
+
+    this.button.onInputOver.add(this.over, this);
+    this.button.onInputOut.add(this.out, this);
+    this.button.onInputUp.add(this.up, this);
+    // this.sprite = this.game.add.sprite(5, 5, 'buttonlevel1');
+    // this.sprite.inputEnabled = true;
+    //
+    // this.sprite.events.onInputDown.add(this.clickListener, this);
   },
   update: function() {
 
   },
+
+  up: function() {
+    console.log('button up', arguments);
+  },
+
+  over: function() {
+    console.log('button over');
+  },
+
+  out: function() {
+    console.log('button out');
+  },
+
   clickListener: function() {
     this.game.state.start('shooter');
   }
